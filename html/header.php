bacd7e1a390ad8c3f206ed9441028a0684e071ed
Margotstdr <margot_studer@yahoo.com>
2026-02-20 16:45:43 +0100

    Co-authored-by: armennce <armennce@users.noreply.github.com> Co-authored-by: Def7899 <Def7899@users.noreply.github.com>

---
+++ html/header.php
@@ -0,0 +1,17 @@
+<?php
+    echo '
+        <header class="header">
+            <div class="logo">
+                <h1>EFREI</h1>
+            </div>
+
+            <nav class="nav">
+                <a href="accueil.php">Accueil</a>
+                <a href="cours.php">Cours + formations</a>
+                <a href="enseignants.php">Enseignants</a>
+                <a href="quiz.php">Quiz formation</a>
+                <a href="plan.php">Plan interactif</a>
+            </nav>
+        </header>
+    ;
+?>

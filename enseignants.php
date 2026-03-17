29846d8a2374ff076fd623a7ae2c8fa3a7cd28e1
armennce <151045937+armennce@users.noreply.github.com>
2026-03-17 09:30:53 +0100

    Add files via upload
    enseignant1

---
+++ enseignants.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php include 'header.php'; ?>
+<body>
+    <h1>Nos enseignants</h1>
+    <p>Découvrez les profils de nos enseignants passionnés et expérimentés, prêts à vous guider dans votre parcours d\'apprentissage à l\'EFREI.</p>
+    <div class="carousel-container">
+  <div class="carousel-track">
+  <section class="profile-card">
+      <img src="prof1.jpg" alt="Nom du prof">
+      <h3>Yulliwas Ameur</h3>
+      <p>Spécialité : Enseignant-chercheur sur l\'axe de recherche Sécurité </p>
+    </section>
+    <section class="profile-card">
+      <img src="prof2.jpg" alt="Nom du prof">
+      <h3>Cherifa Ben Khelil</h3>
+      <p>Spécialité : Enseignate-chercheuse en informatique</p>
+    </section>
+    <section class="profile-card">
+      <img src="prof3.jpg" alt="Nom du prof">
+      <h3>Asma Gabis</h3>
+      <p>Spécialité : 
+    <div class="profile-card">
+      <img src="prof1.jpg" alt="Nom du prof">
+      <h3>Dr. Alan Turing</h3>
+      <p>Spécialité : Algorithmique</p>
+    </div>
+
+    <div class="profile-card">
+      <img src="prof2.jpg" alt="Nom du prof">
+      <h3>Grace Hopper</h3>
+      <p>Spécialité : Systèmes & Compilateurs</p>
+    </div>
+
+    </div>
+
+  <button class="prev">Précédent</button>
+  <button class="next">Suivant</button>
+</div>
+</body>
+</html>
+
+  

a122ce40641869e0ba97ffd5b8e11187b23e4db9
armennce <ratsimbazafyarmence@gmail.com>
2026-03-17 12:58:52 +0100

    déplacement fichier enseignant + creation fichier connexion.php et base de données connexion

---
+++ html/enseignants.php
@@ -0,0 +1,30 @@
+<div class="carousel-container">
+    <div class="carousel-track">
+        <section class="profile-card">
+            <img src="prof1.jpg" alt="Yulliwas Ameur">
+            <h3>Yulliwas Ameur</h3>
+            <p>Spécialité : Enseignant-chercheur sur l'axe de recherche Sécurité</p>
+        </section>
+
+        <section class="profile-card">
+            <img src="prof2.jpg" alt="Cherifa Ben Khelil">
+            <h3>Cherifa Ben Khelil</h3>
+            <p>Spécialité : Enseignante-chercheuse en informatique</p>
+        </section>
+
+        <section class="profile-card">
+            <img src="prof3.jpg" alt="Asma Gabis">
+            <h3>Asma Gabis</h3>
+            <p>Spécialité : Enseignante chercheuse et responsable département</p>
+        </section>
+
+        <section class="profile-card">
+            <img src="prof1.jpg" alt="Alan Turing">
+            <h3>Dr. Alan Turing</h3>
+            <p>Spécialité : Algorithmique</p>
+        </section>
+    </div>
+
+    <button class="prev">Précédent</button>
+    <button class="next">Suivant</button>
+</div>

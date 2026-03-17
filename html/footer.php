90b02ae78d763f05b76514c552b5f3be29d0edad
Margotstdr <margot_studer@yahoo.com>
2026-03-17 08:09:35 +0100

    html for accueil, a propos and plan done

---
+++ html/footer.php
@@ -0,0 +1,18 @@
+<?php
+    echo '
+        <footer class="footer">
+            <a href="apropos.php"> A propos </a>
+            <a href="https://www.facebook.com/EfreiParis/"> Facebook </a>
+            <a href="https://x.com/Efrei_Paris"> X (Twitter) </a>
+            <a href="https://www.youtube.com/efrei"> YouTube </a>
+            <a href="https://www.instagram.com/efrei_paris/"> Instagram </a>
+            <a href="https://www.linkedin.com/school/efrei/"> LinkedIn </a>
+            <a href="https://www.tiktok.com/@be.efrei"> TikTok </a>
+            <a href="https://www.twitch.tv/efrei_?lang=fr"> Twitch </a>
+            <h5> Accueil </h5>
+            <p> +33 188 289 000 </p>
+            <p> admissions@efrei.fr </p>
+            <p> 30-32 Avenue de la République, 94800 Villejuif </p>
+        </footer>
+    ';
+?>
